521520d6947a3c924941252c1ec746fbdc10093c
bamboechop <info@bamboechop.at>
2024-03-12 22:55:16 +0100

    set up astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
+import db from '@astrojs/db';
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [db(), tailwind()],
   output: 'hybrid',
   adapter: node({
     mode: 'standalone'
