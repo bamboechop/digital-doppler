0ab77d4717d6d8b85dd5390597e346c938dc2630
bamboechop <info@bamboechop.at>
2024-03-19 21:21:39 +0100

    reactivity via htmx

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,12 @@ export default {
 	theme: {
 		extend: {
 			backgroundImage: {
+				'background-black-linen': 'url(\'/bg/black-linen.png\')',
 				'background-pattern': 'var(--background-image-url)',
 				'background-fabric': 'url(\'/bg/black-linen.png\')',
+			},
+			dropShadow: {
+				'stationery': '4px 4px 4px rgba(0, 0, 0, .5)',
 			},
 		},
 	},
